5a7c6309b1b2a3bf9a1aaea9b8bd0648b66bd970
Mazzega_Dev <=>
2025-12-10 08:54:57 -0300

    Dia 6

---
+++ secaoTypeScript-24/aulas/src/Secao29/modules/atributos_opcionais.ts
@@ -0,0 +1,20 @@
+// class User {
+   
+
+//    constructor(private name: string, private age: number, private bio?: string){
+
+//    }
+
+//    setBio(bio: string): void{
+//       this.bio = bio
+//    }
+
+//    getBio(): string{
+//       return this.bio || "Biografia indisponivel";
+//    }
+// }
+
+// const user = new User('Joana', 24);
+// console.log(user.getBio());
+// user.setBio('Developer');
+// console.log(user.getBio())
