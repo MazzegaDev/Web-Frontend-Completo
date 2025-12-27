330e09bd72b9563312e3ac5b6b25f5875cf12b9e
Mazzega_Dev <=>
2025-12-27 18:47:57 -0300

    Dia 9

---
+++ secaoTypeScript-24/aulas/src/Secao30/modules/generics_type_alias_interface.ts
@@ -0,0 +1,30 @@
+function createTable(): HTMLTableElement {
+  const table = document.createElement("table");
+
+  table.setAttribute("border", "1");
+
+  const thead = document.createElement("thead");
+
+  const headerRow = document.createElement("tr");
+  const th = document.createElement("th");
+  th.textContent = "Titulo";
+
+  headerRow.appendChild(th);
+
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  const tbody = document.createElement("tbody");
+  const bodyRow = document.createElement("tr");
+  const td = document.createElement("td");
+  td.textContent = "Conteudo";
+
+  bodyRow.appendChild(td);
+  tbody.appendChild(bodyRow);
+  table.appendChild(tbody);
+
+  return table;
+}
+
+const table = createTable();
+document.body.appendChild(table);
