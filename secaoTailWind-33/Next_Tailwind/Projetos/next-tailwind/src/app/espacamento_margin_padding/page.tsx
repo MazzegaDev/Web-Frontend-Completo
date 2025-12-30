4856b05fa799ceb1948da7dc18f651b508392e1f
Mazzega_Dev <=>
2025-12-30 09:32:10 -0300

    Dia 11

---
+++ secaoTailWind-33/Next_Tailwind/Projetos/next-tailwind/src/app/espacamento_margin_padding/page.tsx
@@ -0,0 +1,22 @@
+export default function Espacing(){
+   return (
+      <div>
+         <div className="bg-blue-400 p-2">Padding geral</div>
+         <div className="bg-red-400 pt-2">Padding top</div>
+         <div className="bg-yellow-400 pr-2">Padding right</div>
+         <div className="bg-green-400 pl-2">Padding left</div>
+         <div className="bg-yellow-400 pb-2">Padding bottom</div>
+         <div className="bg-lime-400 px-2">Padding Eixo X</div>
+         <div className="bg-slate-400 py-2">Padding Eixo Y</div>
+         <div className="bg-amber-400 pr-[50px]">Padding Personalizado</div>
+         <div className="bg-blue-400 m-2">Margin geral</div>
+         <div className="bg-red-400 mt-2">Margin top</div>
+         <div className="bg-yellow-400 mr-2">Margin right</div>
+         <div className="bg-green-400 ml-2">Margin left</div>
+         <div className="bg-yellow-400 mb-2">Margin bottom</div>
+         <div className="bg-lime-400 mx-2">Margin Eixo X</div>
+         <div className="bg-slate-400 my-2">Margin Eixo Y</div>
+         <div className="bg-amber-400 mr-[50px]">Margin Personalizado</div>
+      </div>
+   );
+}
